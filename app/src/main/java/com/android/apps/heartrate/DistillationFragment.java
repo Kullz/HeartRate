a7c181b080f809cec71ac0a81ec69ac44ea0709a
kull <kulianichev@gmail.com>
2016-04-05 22:12:09 +0200

    temp

---
+++ app/src/main/java/com/android/apps/heartrate/DistillationFragment.java
@@ -0,0 +1,18 @@
+package com.android.apps.heartrate;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class DistillationFragment extends Fragment {
+
+
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_distillation, container, false);
+
+        return view;
+    }
+}
